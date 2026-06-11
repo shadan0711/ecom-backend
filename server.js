910eeb079987fb81d1cdab2e5340fac37d141ab4
Shadan Khan <shadan_mac5@Shadans-MacBook-Air.local>
2026-06-12 00:07:14 +0530

    Removed duplicate path declaration

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
